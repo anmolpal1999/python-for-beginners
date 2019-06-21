986801a360c8c1a8d7541f5f0eec05be4587e724
anmolpal1999 <41437854+anmolpal1999@users.noreply.github.com>
2019-06-22 03:36:11 +0530

    Update sliceForLoop.py

---
+++ slicing-using-for-loop/sliceForLoop.py
@@ -0,0 +1,8 @@
+print('-------------------------------------------------------------------------')
+family=['Anmol','Sis','Papa','Mummy','Chacha']
+print('Now, we are using for loop for Slicing a list')
+#for i in range(len(family)):
+for family in family[0:3]:
+    print(family)
+print('thank you')    
+print('-------------------------------------------------------------------------')
