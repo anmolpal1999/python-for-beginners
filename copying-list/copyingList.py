9470c35dc2219f9f938261b2717e276ba02b4e4a
anmolpal1999 <41437854+anmolpal1999@users.noreply.github.com>
2019-06-25 01:53:18 +0530

    initial commit

---
+++ copying-list/copyingList.py
@@ -0,0 +1,14 @@
+print('-------------------------------------------------------------------------')
+family=['me','sis','Papa','Mummy','Chacha']
+print('Now, we will copy family list to a another list')
+
+for i in range(len(family)):
+	cpy_family=family[1:4]
+print(family)
+print('--------------------------------------')
+print(cpy_family)
+print('--------------------------------------')
+cpy_family.append('Chachi')
+print(cpy_family)
+print('thank you')    
+print('-------------------------------------------------------------------------')
