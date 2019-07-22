3dbdf3f47128ad18cb0f22c16ca8ee7338a9aa8a
anmolpal1999 <41437854+anmolpal1999@users.noreply.github.com>
2019-07-22 20:21:24 +0530

    update

---
+++ string-types/stringTypes.py
@@ -0,0 +1,39 @@
+print('-------------------------------------------------------------------------')
+string1='iips davv';
+print(string1.upper());
+
+string2="KHANDWA ROAD";
+print(string2.lower());
+
+print(string1[0]);
+print(string2[0]);
+
+print(string1[0:3]);
+print(string2[0:3]);
+
+string3=("ME","MYSELF","ITSELF","ANMOL");
+print(string3);
+
+print(string1.capitalize());
+
+#string5=[1,2,3,4,5]
+#print(string5.max());
+##print(max(string5));
+#print(string5.min());
+
+string6="ms dhoni";
+print(string6.islower());
+
+string7="MS DHONI";
+print(string7.isupper());
+
+string8="123456";
+print(string8.isdigit());
+
+string9="wow";
+print(string9.isalpha());
+
+string10=" cr7 cr7 ";
+print(string10.isspace());
+
+print('-------------------------------------------------------------------------')
